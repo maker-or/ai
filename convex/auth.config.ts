f62ce774b29d8a2dc7346cf5058f48ff112c95ae
Harshith Pasupuleti <harshithpasupuleti@example.com>
2025-06-18 11:23:02 +0530

    updated config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.NEXT_PUBLIC_CONVEX_URL,
+      domain: process.env.VITE_CONVEX_URL || process.env.NEXT_PUBLIC_CONVEX_URL,
       applicationID: "convex",
     },
   ],
